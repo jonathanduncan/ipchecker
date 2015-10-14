7446c85368c4424500bfa9cf2ea50bd6f1c8a507
jonathan <jonathanjosephduncan@gmail.com>
2015-10-14 16:04:08 -0700

    The object oriented implementation is complete and it works nicely. Finishing touches and documentation still needed.

--- IPCLib.py
+++ IPCLib.py
@@ -1,5 +1,6 @@
 __author__ = 'jonathan'
 
+import smtplib as email
 import urllib2 as ulib
 import datetime
 import os
@@ -11,8 +12,8 @@ class PublicIp:
 
     """
     def __init__(self):
-        self.__publicIp = '0.0.0.0'
-        self.__timestamp = ''
+        self.__publicIp = ulib.urlopen('http://www.ipecho.net/plain').read()
+        self.__timestamp = str(datetime.datetime.now())
 
         self.__getpublicip()
         self.__gettimestamp()
@@ -36,6 +37,9 @@ class FileWorks:
         self._path = path
         self._name = name
         self._completePath = self._path + self._name
+        self._filehandle = ''
+
+        self.create()
 
     def openFileR(self):
         '''Return the handle of the file in READONLY mode.'''
@@ -55,14 +59,34 @@ class FileWorks:
     def writeLine(self, line):
         '''Inser line into the file.'''
         with open(self._completePath, 'a') as handle:
-            handle.write(line)
+            handle.write(line + '\n')
 
     def readLastLine(self):
         '''Return only the most receant entry in the IPLog.'''
-        for line in self.openFileR(self):
-            pass
-        return str(line).strip('\n')
+        with open(self._completePath, 'r') as handle:
+            for line in handle:
+                pass
+            return str(line).strip('\n').split(',')
 
-    def readAll(self):
-        '''Return everything in the IPLog.'''
-        return self.openFileR()
+class emailer:
+    def __init__(self):
+        self._toAddress = ""
+        self._fromAddress = ""
+        self._message = ""
+
+        self.server = email.SMTP('smtp.gmail.com', 587)
+        self.server.starttls()
+        if not os.path.isfile("creds"):
+            print "No creds file, can't login to gmail."
+        else:
+            with open("creds") as creds:
+                lines = creds.readlines()
+            self.server.login(lines[0].rstrip(), lines[1].rstrip())
+
+    def compose(self, message, to_address="<jonathanjosephduncan@gmail.com>", from_address="<jonathanjosephduncan@gmail.com>"):
+        self._toAddress = to_address
+        self._fromAddress = from_address
+        self._message = message
+
+    def send(self):
+        self.server.sendmail(self._fromAddress, self._toAddress, self._message)
--- ipc.py
+++ ipc.py
@@ -3,5 +3,12 @@ __author__ = 'jonathan'
 import IPCLib
 
 ip = IPCLib.PublicIp()
+log = IPCLib.FileWorks()
 
-print ip.ip_ts()
+if log.readLastLine()[0] == ip.report()[0]:
+    log.writeLine(', '.join(ip.report()))
+
+    notifyme = IPCLib.emailer()
+    with open("emailbody") as body:
+        notifyme.compose(body.read() + ' ' + ip.report()[0])
+        notifyme.send()
